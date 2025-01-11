d6b4310cea9b40d4f88c0a40e8ec1c995ba21fe4
TickTockBent <benttick@gmail.com>
2025-01-11 14:12:23 -0500

    Add attack type configuration to ReentrancyMock for flexible attack simulations

--- contracts/ReentrancyMock.sol
+++ contracts/ReentrancyMock.sol
@@ -4,21 +4,30 @@ import "./TimeTickBase.sol";
 
 contract ReentrancyMock {
     TimeTickBase public ttb;
+    uint8 public attackType;
     bool public attacking;
     
     constructor(address _ttb) {
         ttb = TimeTickBase(_ttb);
     }
     
-    function attackStake() external {
-        attacking = true;
-        ttb.stake(3600 ether);
-        attacking = false;
+    // Attack types:
+    // 1 = stake
+    // 2 = renew
+    // 3 = unstake
+    // 4 = cancel
+    // 5 = claim
+    function setAttackType(uint8 _type) external {
+        attackType = _type;
     }
     
     function onERC20Received(address, uint256) external returns (bool) {
         if (attacking) {
-            ttb.stake(3600 ether);
+            if (attackType == 1) ttb.stake(3600 ether);
+            if (attackType == 2) ttb.renewStake();
+            if (attackType == 3) ttb.unstake();
+            if (attackType == 4) ttb.cancelUnstake();
+            if (attackType == 5) ttb.claimRewards();
         }
         return true;
     }
