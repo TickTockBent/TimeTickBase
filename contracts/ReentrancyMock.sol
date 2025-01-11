cfc7109b5d8506d51b1eb89511b2df082a1ef740
TickTockBent <benttick@gmail.com>
2025-01-11 14:19:18 -0500

    Add attack type configuration and update onERC20Received in ReentrancyMock for flexible attack handling

--- contracts/ReentrancyMock.sol
+++ contracts/ReentrancyMock.sol
@@ -1,38 +1,73 @@
-// Additional functions in ReentrancyMock
-function stake(uint256 amount) external {
-    ttb.stake(amount);
-}
+pragma solidity ^0.8.28;
 
-function requestUnstake(uint256 amount) external {
-    ttb.requestUnstake(amount);
-}
+import "./TimeTickBase.sol";
 
-function attackStake() external {
-    attacking = true;
-    ttb.stake(3600 ether);
-    attacking = false;
-}
+contract ReentrancyMock {
+    TimeTickBase public ttb;
+    uint8 public attackType;
+    bool public attacking;
+    
+    constructor(address _ttb) {
+        ttb = TimeTickBase(_ttb);
+    }
+    
+    // Attack types:
+    // 1 = stake
+    // 2 = renew
+    // 3 = unstake
+    // 4 = cancel
+    // 5 = claim
+    function setAttackType(uint8 _type) external {
+        attackType = _type;
+    }
+    
+    function onERC20Received(address, uint256) external returns (bool) {
+        if (attacking) {
+            if (attackType == 1) ttb.stake(3600 ether);
+            if (attackType == 2) ttb.renewStake();
+            if (attackType == 3) ttb.unstake();
+            if (attackType == 4) ttb.cancelUnstake();
+            if (attackType == 5) ttb.claimRewards();
+        }
+        return true;
+    }
 
-function attackRenew() external {
-    attacking = true;
-    ttb.renewStake();
-    attacking = false;
-}
+    // Additional functions in ReentrancyMock
+    function stake(uint256 amount) external {
+        ttb.stake(amount);
+    }
 
-function attackUnstake() external {
-    attacking = true;
-    ttb.unstake();
-    attacking = false;
-}
+    function requestUnstake(uint256 amount) external {
+        ttb.requestUnstake(amount);
+    }
 
-function attackCancel() external {
-    attacking = true;
-    ttb.cancelUnstake();
-    attacking = false;
-}
+    function attackStake() external {
+        attacking = true;
+        ttb.stake(3600 ether);
+        attacking = false;
+    }
 
-function attackClaim() external {
-    attacking = true;
-    ttb.claimRewards();
-    attacking = false;
+    function attackRenew() external {
+        attacking = true;
+        ttb.renewStake();
+        attacking = false;
+    }
+
+    function attackUnstake() external {
+        attacking = true;
+        ttb.unstake();
+        attacking = false;
+    }
+
+    function attackCancel() external {
+        attacking = true;
+        ttb.cancelUnstake();
+        attacking = false;
+    }
+
+    function attackClaim() external {
+        attacking = true;
+        ttb.claimRewards();
+        attacking = false;
+    }
 }
