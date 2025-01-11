d9f7ce292936037558132ae123a8181222866071
TickTockBent <benttick@gmail.com>
2025-01-11 14:17:52 -0500

    Add attack functions to ReentrancyMock for direct staking and unstaking actions

--- contracts/ReentrancyMock.sol
+++ contracts/ReentrancyMock.sol
@@ -1,34 +1,38 @@
-pragma solidity ^0.8.28;
+// Additional functions in ReentrancyMock
+function stake(uint256 amount) external {
+    ttb.stake(amount);
+}
 
-import "./TimeTickBase.sol";
+function requestUnstake(uint256 amount) external {
+    ttb.requestUnstake(amount);
+}
 
-contract ReentrancyMock {
-    TimeTickBase public ttb;
-    uint8 public attackType;
-    bool public attacking;
-    
-    constructor(address _ttb) {
-        ttb = TimeTickBase(_ttb);
-    }
-    
-    // Attack types:
-    // 1 = stake
-    // 2 = renew
-    // 3 = unstake
-    // 4 = cancel
-    // 5 = claim
-    function setAttackType(uint8 _type) external {
-        attackType = _type;
-    }
-    
-    function onERC20Received(address, uint256) external returns (bool) {
-        if (attacking) {
-            if (attackType == 1) ttb.stake(3600 ether);
-            if (attackType == 2) ttb.renewStake();
-            if (attackType == 3) ttb.unstake();
-            if (attackType == 4) ttb.cancelUnstake();
-            if (attackType == 5) ttb.claimRewards();
-        }
-        return true;
-    }
+function attackStake() external {
+    attacking = true;
+    ttb.stake(3600 ether);
+    attacking = false;
+}
+
+function attackRenew() external {
+    attacking = true;
+    ttb.renewStake();
+    attacking = false;
+}
+
+function attackUnstake() external {
+    attacking = true;
+    ttb.unstake();
+    attacking = false;
+}
+
+function attackCancel() external {
+    attacking = true;
+    ttb.cancelUnstake();
+    attacking = false;
+}
+
+function attackClaim() external {
+    attacking = true;
+    ttb.claimRewards();
+    attacking = false;
 }
