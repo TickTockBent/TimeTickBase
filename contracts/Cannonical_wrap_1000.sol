81a3d4ba7d2e147152546aee8f4682c78b6c3248
TickTockBent <benttick@gmail.com>
2025-01-13 09:40:05 -0500

    Add TTBWrapper1000 contract and deployment script

---
+++ contracts/Cannonical_wrap_1000.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+interface ITTB is IERC20 {}
+
+contract TTBWrapper1000 is ERC20, ReentrancyGuard {
+    ITTB public immutable TTB;
+    uint256 public immutable wrapRatio; // How many wrapped tokens per 1 TTB
+    bool public immutable allowUnwrap;
+
+    event Wrapped(address indexed user, uint256 ttbAmount, uint256 wrappedAmount);
+    event Unwrapped(address indexed user, uint256 wrappedAmount, uint256 ttbAmount);
+
+    constructor(
+        address ttbAddress,
+        string memory name,
+        string memory symbol,
+        uint256 _wrapRatio,
+        bool _allowUnwrap
+    ) ERC20(name, symbol) {
+        TTB = ITTB(ttbAddress);
+        wrapRatio = _wrapRatio;
+        allowUnwrap = _allowUnwrap;
+    }
+
+    function wrap(uint256 ttbAmount) external nonReentrant {
+        require(ttbAmount > 0, "Amount must be > 0");
+        
+        uint256 wrappedAmount = ttbAmount * wrapRatio;
+        
+        // Transfer TTB from user to this contract
+        require(TTB.transferFrom(msg.sender, address(this), ttbAmount), "TTB transfer failed");
+        
+        // Mint wrapped tokens to user
+        _mint(msg.sender, wrappedAmount);
+        
+        emit Wrapped(msg.sender, ttbAmount, wrappedAmount);
+    }
+
+    function unwrap(uint256 wrappedAmount) external nonReentrant {
+        require(allowUnwrap, "Unwrapping not allowed");
+        require(wrappedAmount > 0, "Amount must be > 0");
+        
+        uint256 ttbAmount = wrappedAmount / wrapRatio;
+        require(ttbAmount > 0, "TTB amount too small");
+        
+        // Burn wrapped tokens
+        _burn(msg.sender, wrappedAmount);
+        
+        // Return TTB to user
+        require(TTB.transfer(msg.sender, ttbAmount), "TTB transfer failed");
+        
+        emit Unwrapped(msg.sender, wrappedAmount, ttbAmount);
+    }
+}
---
+++ deploy/01_deploy_Canonical_wrap_1000.ts
@@ -0,0 +1,29 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+const deployWrapper: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  // Get the TTB contract address from previous deployment
+  const TTB = await deployments.get('TimeTickBase');
+
+  // Example wrapper parameters
+  const NAME = "Wrapped TTB";
+  const SYMBOL = "wTTB";
+  const WRAP_RATIO = 1000; // 1 TTB = 1000 wTTB
+  const ALLOW_UNWRAP = true;
+
+  await deploy('TTBWrapper1000', {
+    from: deployer,
+    args: [TTB.address, NAME, SYMBOL, WRAP_RATIO, ALLOW_UNWRAP],
+    log: true,
+    waitConfirmations: 1,
+  });
+};
+
+deployWrapper.tags = ['TTBWrapper1000'];
+deployWrapper.dependencies = ['TimeTickBase'];
+
+export default deployWrapper;
